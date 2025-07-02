4b0251f56e62f20d617ea13a6da354b9a72aee33
victormedeiros1 <josevictordev@gmail.com>
2025-07-02 00:26:38 -0300

    refactor: change font

--- src/App.vue
+++ src/App.vue
@@ -10,9 +10,11 @@ const text = ref<string>("");
 <template>
   <Container>
     <div class="header">
-      <div class="header__title">
-        <h1>ALFASIN</h1>
-      </div>
+      <h1 class="header__title">ALFASIN</h1>
+      <p class="header__description">
+        Bem-vindo ao Alfasin, um conversor de alfabeto escrito para alfabeto de
+        libras.
+      </p>
       <div class="header__controls"></div>
     </div>
     <div class="content">
@@ -23,10 +25,18 @@ const text = ref<string>("");
 </template>
 
 <style scoped>
-h1 {
+.header {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.header__title {
   font-size: 4rem;
 }
-
+.header__description {
+  font-size: 1.25rem;
+  margin: -1rem 0 1rem 0;
+}
 .content {
   width: 100%;
   max-width: 1000px;
